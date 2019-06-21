d734a140d2a6d3b6176dd06d3a991bc0c776b27b
justine <justine.telmann@gmail.com>
2019-06-21 09:47:43 +0200

    ajout du form

--- src/Controller/ImmoController.php
+++ src/Controller/ImmoController.php
@@ -4,8 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ImmoController extends AbstractController
 {
@@ -22,14 +27,44 @@ class ImmoController extends AbstractController
             'articles' => $articles,
         ]);
     }
+
     /**
-     * @route ("/", name="home")
+     * @route("/", name="home")
      */
     public function home(){
         return $this->render('immo/home.html.twig', [
             'title' => "Bienvenue sur le site d'immobilier"
         ]);
     }
+
+    /**
+     * @route("/immo/new", name="immo_create")
+     */
+    public function create(Request $request, objectManager $manager){
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+        ->add('title', TextType::class,[
+            'attr' => [
+                'placeholder' => 'nom du bien']])
+            
+        
+        ->add('image', TextType::class,[
+            'attr' => [
+                'placeholder' => 'image']])
+
+        ->add('content', TextareaType::class,[
+            'attr' => [
+                'placeholder' => 'descriptif']])
+
+        ->getForm();
+
+
+        return $this->render('immo/create.html.twig',[
+            'formArticle' => $form->createView()
+        ]);
+    }
+
     /**
      * @route ("/immo/{id}", name="immo_show")
      */
